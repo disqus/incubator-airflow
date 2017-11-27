47560328a7745cf45b6ff6dfea69fe6fe2323ff4
Trevor Joynson (trevorj) <github@skywww.net>
2017-11-27 12:44:29 -0800

    Just avoid that alltogether to clean up these logs.

--- airflow/utils/net.py
+++ airflow/utils/net.py
@@ -1,5 +1,9 @@
-from airflow.configuration import conf, AirflowConfigException
+from airflow import configuration
+from airflow.configuration import conf
 import socket
+
+
+_sentinel = object()
 
 
 def get_hostname(default=socket.getfqdn):
@@ -8,6 +12,12 @@ def get_hostname(default=socket.getfqdn):
 
     :param callable|str default: Default if config does not specify. If a callable is given it will be called.
     """
-    fallback = callable(default) and default() or default
-    hostname = conf.get('core', 'hostname', fallback=fallback)
+
+    hostname = _sentinel
+    if configuration.has_option('core', 'hostname'):
+        hostname = conf.get('core', 'hostname') or _sentinel
+
+    if hostname is _sentinel:
+        hostname = callable(default) and default() or default
+
     return hostname
